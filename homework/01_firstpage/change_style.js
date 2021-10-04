c2042cb0f44a8c5ed6c10fd13bfa03fdc26ec873
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-10-04 18:16:21 +0400

    For of заменены на forEach во всех случаях

--- homework/01_firstpage/change_style.js
+++ homework/01_firstpage/change_style.js
@@ -22,72 +22,30 @@ function changeTheme() {
         header.classList.add('header_theme_dark');
         sideBar.classList.add('side-bar_theme_dark');
         main.classList.add('main_theme_dark');
-
-        for (let block of typeBlocks) {
-            block.classList.add('type-container__block_theme_dark');
-        }
-
-        for (let text of blockTexts) {
-            text.classList.add('block-text_theme_dark');
-        }
-
-        for (let link of blockLinks) {
-            link.classList.add('block__link_theme_dark');
-        }
-
-        for (let block of fishBlocks) {
-            block.classList.add('fish-container__block_theme_dark');
-        }
-
-        for (let button of buyButtons) {
-            button.classList.add('block__buy_theme_dark');
-        }
-
-        for (let link of buyLinks) {
-            link.classList.add('buy__link_theme_dark');
-        }
-
         footer.classList.add('footer_theme_dark');
         footerLink.classList.add('footer__link_theme_dark');
 
-        for (let text of footerTexts) {
-            text.classList.add('footer__text_theme_dark');
-        }
+        typeBlocks.forEach( (item) => item.classList.add('type-container__block_theme_dark') );
+        blockTexts.forEach( (item) => item.classList.add('block-text_theme_dark') );
+        blockLinks.forEach( (item) => item.classList.add('block__link_theme_dark') );
+        fishBlocks.forEach( (item) => item.classList.add('fish-container__block_theme_dark') );
+        buyButtons.forEach( (item) => item.classList.add('block__buy_theme_dark') );
+        buyLinks.forEach( (item) => item.classList.add('buy__link_theme_dark') );
+        footerTexts.forEach( (item) => item.classList.add('footer__text_theme_dark') );
     } else {
         body.classList.remove('body_theme_dark');
         header.classList.remove('header_theme_dark');
         sideBar.classList.remove('side-bar_theme_dark');
         main.classList.remove('main_theme_dark');
-
-        for (let block of typeBlocks) {
-            block.classList.remove('type-container__block_theme_dark');
-        }
-
-        for (let text of blockTexts) {
-            text.classList.remove('block-text_theme_dark');
-        }
-
-        for (let link of blockLinks) {
-            link.classList.remove('block__link_theme_dark');
-        }
-
-        for (let block of fishBlocks) {
-            block.classList.remove('fish-container__block_theme_dark');
-        }
-
-        for (let button of buyButtons) {
-            button.classList.remove('block__buy_theme_dark');
-        }
-
-        for (let link of buyLinks) {
-            link.classList.remove('buy__link_theme_dark');
-        }
-
         footer.classList.remove('footer_theme_dark');
         footerLink.classList.remove('footer__link_theme_dark');
 
-        for (let text of footerTexts) {
-            text.classList.remove('footer__text_theme_dark');
-        }
+        typeBlocks.forEach( (item) => item.classList.remove('type-container__block_theme_dark') );
+        blockTexts.forEach( (item) => item.classList.remove('block-text_theme_dark') );
+        blockLinks.forEach( (item) => item.classList.remove('block__link_theme_dark') );
+        fishBlocks.forEach( (item) => item.classList.remove('fish-container__block_theme_dark') );
+        buyButtons.forEach( (item) => item.classList.remove('block__buy_theme_dark') );
+        buyLinks.forEach( (item) => item.classList.remove('buy__link_theme_dark') );
+        footerTexts.forEach( (item) => item.classList.remove('footer__text_theme_dark') );
     }
 }
