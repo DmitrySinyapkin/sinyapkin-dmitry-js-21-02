be52afaac6c28538c3e836bb311f45cb921994fe
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-10-18 20:23:51 +0400

    Добавление изображений в галерею перенесено в utils.js

--- homework/10_binary_storages/index.js
+++ homework/10_binary_storages/index.js
@@ -1,10 +1,6 @@
 import { uploadToImgBB } from "./api/imgbbapi.js";
-import { addImgToGallery } from "./utils/utils.js";
 
 const input = document.querySelector('.file-input');
 const uploadButton = document.querySelector('.upload-button');
 
 uploadButton.addEventListener('click', () => uploadToImgBB(input.files[0]));
-
-const imgArr = localStorage.getItem('imgArr') ? JSON.parse(localStorage.getItem('imgArr')) :[];
-imgArr.forEach(imgUrl => addImgToGallery(imgUrl));
