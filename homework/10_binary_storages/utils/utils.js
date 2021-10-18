c80d0b4f4bb88dc87086cff1867c020c870a530a
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-10-18 20:26:02 +0400

    Добавление изображений в галерею перенесено из index.js

--- homework/10_binary_storages/utils/utils.js
+++ homework/10_binary_storages/utils/utils.js
@@ -16,8 +16,11 @@ function saveToLocalStorage(data) {
     localStorage.setItem('imgArr', JSON.stringify(imgArr));
 }
 
-export function addImgToGallery(url) {
+function addImgToGallery(url) {
     const img = document.createElement('img');
     img.src = url;
     document.querySelector('.gallery').append(img);
 }
+
+const imgArr = localStorage.getItem('imgArr') ? JSON.parse(localStorage.getItem('imgArr')) :[];
+imgArr.forEach(imgUrl => addImgToGallery(imgUrl));
