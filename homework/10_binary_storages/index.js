9c561f464b224d8322c89ba695cd78b5788587af
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-10-18 19:57:20 +0400

    Код вынесен в модули

--- homework/10_binary_storages/index.js
+++ homework/10_binary_storages/index.js
@@ -1,25 +1,10 @@
+import { uploadToImgBB } from "./api/imgbbapi.js";
+import { addImgToGallery } from "./utils/utils.js";
+
 const input = document.querySelector('.file-input');
 const uploadButton = document.querySelector('.upload-button');
-const gallery = document.querySelector('.gallery');
 
-uploadButton.addEventListener('click', uploadToImgBB);
+uploadButton.addEventListener('click', () => uploadToImgBB(input.files[0]));
 
-function uploadToImgBB(file) {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => {
-        const formData = new FormData();
-        formData.set('key', '3f5e5f5b3b0ff6c97175f677df6726d2');
-        formData.set('image', reader.result.replace(/^.*,/, ''))
-    }
-}
-
-const createFetch = (url, data) => (callback, errorCallback) => {
-    fetch(url, {
-        method: 'POST',
-        body: data
-    })
-        .then(response => response.json())
-        .then(callback)
-        .catch(errorCallback)
-}
+const imgArr = localStorage.getItem('imgArr') ? JSON.parse(localStorage.getItem('imgArr')) :[];
+imgArr.forEach(imgUrl => addImgToGallery(imgUrl));
