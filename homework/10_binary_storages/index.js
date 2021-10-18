3f0760c5948663a9ed9b6e7f41d8ecf9ce38bf7a
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-10-18 21:04:46 +0400

    Исправлен метод imgbb.upload

--- homework/10_binary_storages/index.js
+++ homework/10_binary_storages/index.js
@@ -1,6 +1,6 @@
-import { uploadToImgBB } from "./api/imgbbapi.js";
+import { imgbbapi } from "./api/imgbbapi.js";
 
 const input = document.querySelector('.file-input');
 const uploadButton = document.querySelector('.upload-button');
 
-uploadButton.addEventListener('click', () => uploadToImgBB(input.files[0]));
+uploadButton.addEventListener('click', () => imgbbapi.upload(input.files[0]));
