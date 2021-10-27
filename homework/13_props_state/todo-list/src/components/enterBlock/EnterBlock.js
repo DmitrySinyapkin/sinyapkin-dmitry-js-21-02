ddb1b633664e9d004ee28bab49943de4e1298a7c
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-10-27 20:07:06 +0400

    Визуальные правки

--- homework/13_props_state/todo-list/src/components/enterBlock/EnterBlock.js
+++ homework/13_props_state/todo-list/src/components/enterBlock/EnterBlock.js
@@ -5,7 +5,7 @@ export class EnterBlock extends React.Component {
     render() {
         return (
             <div className="enter-container">
-                <p>Ввод текста задачи</p>
+                <p>Нужно сделать</p>
                 <div className="line-container">
                     <input type="text" className="input" />
                     <button className="add-button">Добавить</button>
