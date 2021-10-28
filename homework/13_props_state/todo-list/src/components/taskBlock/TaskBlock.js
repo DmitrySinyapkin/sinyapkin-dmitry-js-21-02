0b7714433de216256a225d06ae360621390902a0
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-10-28 15:40:03 +0400

    Добавлена возможность помечать задачу выполненной

--- homework/13_props_state/todo-list/src/components/taskBlock/TaskBlock.js
+++ homework/13_props_state/todo-list/src/components/taskBlock/TaskBlock.js
@@ -2,13 +2,23 @@ import React from "react";
 import './TaskBlock.css';
 
 export class TaskBlock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {done: false};
+        this.markDone = this.markDone.bind(this);
+    }
+
+    markDone() {
+        this.setState({done: true});
+    }
+
     render() {
         return (
             <div className="task-block">
                 <div className="task-check">
-                    <input type="checkbox" />
+                    <input type="checkbox" className={this.state.done && 'hidden'} onClick={this.markDone}/>
                 </div>
-                <div className="task-text">{this.props.text}</div>
+                <div className={`task-text ${this.state.done && 'done'}`}>{this.props.text}</div>
                 <div className="task-delete">
                     <div className="del-btn">X</div>
                 </div>
