9d498d382d5fb2d3336c1da55be8a2469cb4c4d8
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-11-02 14:10:18 +0400

    Переписаны App и UserList

--- homework/15_function_components/dummy/src/App.js
+++ homework/15_function_components/dummy/src/App.js
@@ -4,28 +4,25 @@ import { Header } from './components/Header/Header';
 import { UserList } from './components/UserList/UserList';
 import { ThemeContextConsumer } from './contexts/ThemeContext';
 
-export class App extends React.Component {
-  render() {
-    return (
-      <ThemeContextConsumer>
-        {
-          (context) => (
-            <div className={`App ${context.darkTheme && "App_dark"}`}>
-              <div className="body">
-                <header>
-                  <Header name="Пользователи"/>
-                </header>
-                <main>
-                  <UserList/>
-                </main>
-              </div>
-            </div>  
-          )  
-        }
-      </ThemeContextConsumer>
-      
+const App = () => {
+  return (
+    <ThemeContextConsumer>
+      {
+        (context) => (
+          <div className={`App ${context.darkTheme && "App_dark"}`}>
+            <div className="body">
+              <header>
+                <Header name="Пользователи"/>
+              </header>
+              <main>
+                <UserList/>
+              </main>
+            </div>
+          </div>  
+        )  
+      }
+    </ThemeContextConsumer>   
     );
   }
-}
 
 export default App;
--- homework/15_function_components/dummy/src/components/UserList/UserList.js
+++ homework/15_function_components/dummy/src/components/UserList/UserList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ComponentWithHelper from "../../wrappers/ComponentWithHelper";
 import { getUserList } from "../../api/dummyApi";
 import { PageButton } from "../PageButton/PageButton";
@@ -6,69 +6,57 @@ import { User } from "../User/User";
 import './UserList.css';
 import { ThemeContextConsumer } from "../../contexts/ThemeContext";
 
-const initialState = {
-    users: [],
-    currentPage: 0,
-}
+export const UserList = () => {
+    const [users, setUsers] = useState([]);
+    const [currentPage, setCurrentPage] = useState(0);
+    const pageNumbers = [0, 1, 2, 3, 4];
 
-const pageNumbers = [0, 1, 2, 3, 4];
-
-export class UserList extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = initialState;
-        this.loadUsers = this.loadUsers.bind(this);
-        this.changePage = this.changePage.bind(this);
+    const loadUsers = (page, limit) => {
+        getUserList(page, limit, response => setUsers(response.data));
     }
 
-    loadUsers(page, limit) {
-        getUserList(page, limit, response => this.setState({users: response.data}));
+    useEffect(() => {
+        loadUsers(0, 10);
+    }); 
+
+    const changePage = (page) => {
+        setCurrentPage(page);
+        loadUsers(page, 10);
     }
 
-    componentDidMount() {
-        this.loadUsers(0, 10);
-    }
-
-    changePage(page) {
-        this.setState({currentPage: page});
-        this.loadUsers(page, 10);
-    }
-
-    render() {
-        return (
-            <div className="userlist">
-                <div className="userlist__container">
-                    {this.state.users.map((item, index) =>
-                        <ComponentWithHelper text={item.id} key={index}>
-                            <User 
-                                title={item.title}
-                                firstName={item.firstName}
-                                lastName={item.lastName}
-                                picture={item.picture}
-                            />
-                        </ComponentWithHelper>
+    return (
+        <div className="userlist">
+            <div className="userlist__container">
+                {users.map((item, index) =>
+                    <ComponentWithHelper text={item.id} key={index}>
+                        <User 
+                            title={item.title}
+                            firstName={item.firstName}
+                            lastName={item.lastName}
+                            picture={item.picture}
+                        />
+                    </ComponentWithHelper>
+                )}
+                </div>
+            <div className="pages__container">
+                <div className="pages__changer">
+                    {pageNumbers.map((item) =>
+                        <PageButton
+                            number={item + 1}
+                            activeNumber={currentPage + 1}
+                            onClick={() => changePage(item)}
+                            key={item}
+                        />
                     )}
                 </div>
-                <div className="pages__container">
-                    <div className="pages__changer">
-                        {pageNumbers.map((item) =>
-                            <PageButton
-                                number={item + 1}
-                                activeNumber={this.state.currentPage + 1}
-                                onClick={() => this.changePage(item)}
-                                key={item}
-                            />
-                        )}
-                    </div>
-                    <ThemeContextConsumer>
-                        {
-                            (context) => (
-                                <label className={context.darkTheme && "label_dark"}>Темная тема  <input className="theme-changer" type="checkbox" checked={context.darkTheme} onClick={context.toggleTheme}/></label> 
-                            )  
-                        }
-                    </ThemeContextConsumer>
-                </div>
+                <ThemeContextConsumer>
+                    {
+                        (context) => (
+                            <label className={context.darkTheme && "label_dark"}>Темная тема  <input className="theme-changer" type="checkbox" checked={context.darkTheme} onClick={context.toggleTheme}/></label> 
+                        )  
+                    }
+                </ThemeContextConsumer>
             </div>
+        </div>
         );
-    }
-}
+    };
