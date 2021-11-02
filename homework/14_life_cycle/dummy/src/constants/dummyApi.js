6bf136e77a134a794c8a6ac58668394aa5abd85d
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-11-02 12:25:26 +0400

    2

--- homework/14_life_cycle/dummy/src/constants/dummyApi.js
+++
@@ -1,7 +0,0 @@
-const BASE_URL = 'https://dummyapi.io/data/v1/';
-
-export const USER_LIST_URL = `${BASE_URL}user`;
-
-export const APP_ID_VALUE = '617d0f3dab5df4b9bd1507dd';
-
-export const APP_ID_FIELD = 'app-id';
