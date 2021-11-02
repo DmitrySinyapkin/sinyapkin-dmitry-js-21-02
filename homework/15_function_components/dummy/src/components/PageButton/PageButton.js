43650e37171f9bc3e42de9320ceb7b68f221aab5
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-11-02 12:45:53 +0400

    Переписан PageButton

--- homework/15_function_components/dummy/src/components/PageButton/PageButton.js
+++ homework/15_function_components/dummy/src/components/PageButton/PageButton.js
@@ -1,17 +1,8 @@
 import React from "react";
 import './PageButton.css';
 
-const initialState = {active: false}
-
-export class PageButton extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = initialState;
-    }
-
-    render() {
-        return (
-            <button className={this.props.activeNumber === this.props.number && 'button_active'} onClick={this.props.onClick}>{this.props.number}</button>
-        );
-    }
-}
+export const PageButton = (props) => {
+    return (
+        <button className={props.activeNumber === props.number && 'button_active'} onClick={props.onClick}>{props.number}</button>
+    );
+};
