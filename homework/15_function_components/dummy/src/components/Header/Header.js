5957ff0c600867026551d6ad0642401141554fd0
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-11-02 13:44:27 +0400

    Переписан Header.js

--- homework/15_function_components/dummy/src/components/Header/Header.js
+++ homework/15_function_components/dummy/src/components/Header/Header.js
@@ -2,18 +2,16 @@ import React from "react";
 import './Header.css';
 import { ThemeContextConsumer } from "../../contexts/ThemeContext";
 
-export class Header extends React.Component {
-    render() {
-        return (
-            <ThemeContextConsumer>
-                {
-                    (context) => (
-                        <div className="header">
-                            <h1 className={`header__text ${context.darkTheme && "header__text_dark"}`}>{this.props.name}</h1>
-                        </div>
-                    )  
-                }
-            </ThemeContextConsumer>
-        );
-    }
-}
+export const Header = (props) => {
+    return (
+        <ThemeContextConsumer>
+            {
+                (context) => (
+                    <div className="header">
+                        <h1 className={`header__text ${context.darkTheme && "header__text_dark"}`}>{props.name}</h1>
+                    </div>
+                )  
+            }
+        </ThemeContextConsumer>
+    );
+};
