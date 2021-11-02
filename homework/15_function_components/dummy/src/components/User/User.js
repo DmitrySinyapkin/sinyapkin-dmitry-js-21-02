a5da5f971df1914b3c81e2cc672ff50255105528
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-11-02 13:50:53 +0400

    Переписан User

--- homework/15_function_components/dummy/src/components/User/User.js
+++ homework/15_function_components/dummy/src/components/User/User.js
@@ -2,23 +2,22 @@ import React from 'react';
 import './User.css';
 import { ThemeContextConsumer } from '../../contexts/ThemeContext';
 
-export class User extends React.Component {
-    render() {
-        return (
-            <ThemeContextConsumer>
-                        {
-                            (context) => (
-                                <div className={`user__container ${context.darkTheme && "user__container_dark"}`}>
-                                    <figure className="user__photo">
-                                        <img src={this.props.picture} alt="Load Error"/>
-                                    </figure>
-                                    <div className={`user__name ${context.darkTheme && "user__name_dark"}`}>
-                                        <span>{this.props.title} {this.props.firstName} {this.props.lastName}</span>
-                                    </div>
-                                </div>
-                            )  
-                        }
-            </ThemeContextConsumer>
+export const User = (props) => {
+    return (
+        <ThemeContextConsumer>
+            {
+                (context) => (
+                    <div className={`user__container ${context.darkTheme && "user__container_dark"}`}>
+                        <figure className="user__photo">
+                            <img src={props.picture} alt="Load Error"/>
+                        </figure>
+                        <div className={`user__name ${context.darkTheme && "user__name_dark"}`}>
+                            <span>{props.title} {props.firstName} {props.lastName}</span>
+                        </div>
+                    </div>
+                )  
+            }
+        </ThemeContextConsumer>
         );
-    }
-}
+    };
+
