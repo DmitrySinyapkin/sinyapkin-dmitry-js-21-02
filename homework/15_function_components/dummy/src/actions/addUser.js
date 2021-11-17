e88a00b1c2a79614cb93e5ef50c38eefcadd8235
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-11-17 16:47:36 +0400

    Форма создания нового юзера переписана с использованием redux

--- homework/15_function_components/dummy/src/actions/addUser.js
+++ homework/15_function_components/dummy/src/actions/addUser.js
@@ -1,8 +1,27 @@
-import dispatcher from "../dispatcher"
+import { addUser } from "../api/dummyApi";
 
-export const addUserAction = (user) => {
-    dispatcher.dispatch({
-        type: 'ADD_USER',
-        payload: user,
-    });
+const addUserAction = (userId) => ({
+    type: 'REGFORM/ADD_USER',
+    userId,
+});
+
+const preventRedirectAction = () => ({
+    type: 'REGFORM/PREVENT_REDIRECT'
+});
+
+export const setNewUserId = (firstName, lastName, email, title, gender, phone) => (dispatch) => {
+    const user = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        title: title,
+        gender: gender,
+        phone: phone
+    };
+
+    addUser(user).then(response => dispatch(addUserAction(response.id)));
+}
+
+export const preventRedirect = () => (dispatch) => {
+    dispatch(preventRedirectAction());
 }
--- homework/15_function_components/dummy/src/store.js
+++ homework/15_function_components/dummy/src/store.js
@@ -2,12 +2,14 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import userDataReducer from './reducers/userData';
 import userListReducer from './reducers/userList';
 import thunk from 'redux-thunk';
+import addUserReducer from './reducers/addUser';
 
 const store = createStore(
     combineReducers(
         {
             users: userListReducer,
             userData: userDataReducer,
+            newUser: addUserReducer,
         },
     ),
     applyMiddleware(thunk),
