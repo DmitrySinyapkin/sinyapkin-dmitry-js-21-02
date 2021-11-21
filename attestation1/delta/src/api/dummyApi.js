e3149fd68b64199f0ba55a9f691d063c3e4479eb
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-11-21 13:03:41 +0400

    Добавлен api

---
+++ attestation1/delta/src/api/dummyApi.js
@@ -0,0 +1,39 @@
+import { USER_LIST_URL, APP_ID_FIELD, APP_ID_VALUE } from "../constants/dummyApi"
+
+async function createGetFetch(url) {
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: new Headers({
+            [APP_ID_FIELD]: APP_ID_VALUE
+        }),
+    });
+    return await response.json();
+}
+
+async function createPostFetch(url, data) {
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: new Headers({
+            [APP_ID_FIELD]: APP_ID_VALUE,
+            'Content-Type': 'application/json;charset=utf-8'
+        }),
+        body: data,
+    });
+    return await response.json();
+}
+
+export const getUserListApi = (page, limit) => {
+    const url = USER_LIST_URL + `?page=${page}&limit=${limit}`;
+    return createGetFetch(url);
+}
+
+export const getUserDataApi = (id) => {
+    const url = USER_LIST_URL + `/${id}`;
+    return createGetFetch(url);
+}
+
+export const addUserApi = (user) => {
+    const url = USER_LIST_URL + `/create`;
+    const userData = JSON.stringify(user);
+    return createPostFetch(url, userData);
+}
