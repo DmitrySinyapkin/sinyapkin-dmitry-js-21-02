0b98e3ee18b9b66d053bcb1115e59d446ef4f1c0
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-12-07 16:43:48 +0400

    Обработка ошибок

--- homework/31_files/server/src/services/fileService.js
+++ homework/31_files/server/src/services/fileService.js
@@ -10,8 +10,12 @@ class FileService {
     }
 
     saveTextToFile(req, res) {
-        FileRepository.saveTextToFile(req.body.text);
-        res.status(200).send('File saved')
+        try {
+            FileRepository.saveTextToFile(req.body.text);
+            res.status(200).send('File saved');
+        } catch(err) {
+            res.status(520).send(err);
+        }
     }
 }
 
