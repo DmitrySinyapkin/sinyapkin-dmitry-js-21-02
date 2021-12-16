e56abb6f67a1a24f3a4ce0512d604b39dbbe4c0d
DmitrySinyapkin <dsinyapkin@mail.ru>
2021-12-16 13:11:20 +0400

    Утилиты преобразования формата дат переписаны с использованием moment js

--- attestation1/server/src/utils/commonUtils.js
+++ attestation1/server/src/utils/commonUtils.js
@@ -1,14 +1,11 @@
+const moment = require('moment');
+
 class CommonUtils {
     changeDateFormat(stringDate) {
-        const date = new Date(Date.parse(stringDate));
-        const day = (date.getDate() < 10) ? '0' + date.getDate() : date.getDate();
-        const month = (date.getMonth() < 10) ? '0' + date.getMonth() : date.getMonth();
-        return `${day}.${month}.${date.getFullYear()}`;
+        return moment(stringDate).format('DD.MM.YYYY');
     }
     dateToIsoString(stringDate) {
-        const arr = stringDate.split('.');
-        const date = new Date(+arr[2], +arr[1] - 1, +arr[0]);
-        return new Date(date.getTime() - (date.getTimezoneOffset() * 60000)).toISOString();
+        return moment(stringDate).format();
     }
 }
 
